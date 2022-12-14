c4e70f0f7fbab8238625396b3f3e0853d2348b2e
Taylor <bKuzzcs@gmail.com>
2022-12-13 23:05:01 -0700

    commit tree bb7da2681296b5af6d971281908962742c11270b

--- js/index.js
+++ js/index.js
@@ -38,11 +38,11 @@ function howsTheWeather(temperature) {
   all other numbers, just return the number itself.
 */
 function fizzbuzz(num) {
-  if (num % 3 === 0 && num % 5 === 0) {
+  if (num % 3 == 0 && num % 5 == 0) {
     return "FizzBuzz";
-  } else if (num % 3 === 0) {
+  } else if (num % 3 == 0) {
     return "Fizz";
-  } else if (num % 5 === 0) {
+  } else if (num % 5 == 0) {
     return "Buzz";
   } else {
     return num;
